c37c3d589a0be411a282084054922d6b4ea1af8d
AmanVerma0903 <vermaman1921@gmail.com>
2025-07-29 00:15:03 +0530

    fix:nltkin app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import streamlit as st
 import pickle
 import string
-from nltk.corpus import stopwords
+import os
 import nltk
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-nltk.download('punkt')
+
+# Fix for Streamlit Cloud: Force NLTK to download to local dir
+nltk_data_path = os.path.join(os.getcwd(), 'nltk_data')
+os.makedirs(nltk_data_path, exist_ok=True)
+nltk.data.path.append(nltk_data_path)
+nltk.download('punkt', download_dir=nltk_data_path)
+
 ps = PorterStemmer()
 
 # Function to transform input text
